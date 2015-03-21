3e58249ff419f0b306adbc9811024aeac2d58867
Hsuan Lee <boczeratul@gmail.com>
2015-03-21 16:24:24 +0800

    initial commit (add lesson 1 and 2)

---
+++ lesson_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c;
+
+	cout << "I'm so smart I know how to add two numbers!!!" << endl;
+    cout << "a = ";
+    cin >> a;
+
+    cout << "b = ";
+    cin >> b;
+
+    c = a + b;
+    cout << "a + b = " << c << endl;
+
+    return 1;
+}
