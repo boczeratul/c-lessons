25d9aca1fb3ac669fdcb4fb74208fbc127d52d72
Hsuan Lee <boczeratul@gmail.com>
2015-03-21 16:33:25 +0800

    add lesson 3

---
+++ lesson_3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, sum = 0;
+
+	cout << "I'm so smart I know how to add from 1 to n!!!" << endl;
+    cout << "n = ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+    	sum += i;
+    }
+
+    cout << "1 + 2 + ... + " << n << " = " << sum << endl;
+
+    return 1;
+}
